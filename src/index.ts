aac1bbd61f925f98f3244a6f2d95e1344facfee5
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-03-08 17:36:39 +0530

    feat: export theme types and default color constants
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { MaplibreInspect } from './maplibre-gl-inspect';
 
 export { MaplibreInspect };
+export { DEFAULT_LIGHT_COLORS, DEFAULT_DARK_COLORS } from './theme-colors';
+export type {
+  Options,
+  RenderPopupFeature,
+  RenderPopupProperty,
+  Theme,
+  ThemeColors,
+} from './types';
 export default MaplibreInspect;
